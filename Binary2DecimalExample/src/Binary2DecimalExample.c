7dda8dc1ebbaec01cb5d798c286979b315fc00b9
James.Westfall <james.westfall@usafa.edu>
2020-02-13 16:38:57 -0700

    completed using a string input

--- Binary2DecimalExample/src/Binary2DecimalExample.c
+++ Binary2DecimalExample/src/Binary2DecimalExample.c
@@ -2,41 +2,48 @@
  * Binary2DecimalExample.c
  *
  *  Created on: Feb 13, 2020
- *      Author: James.Westfall
+ *      Author: James Westfall
  */
 
 #include <stdio.h>
+
+#include <string.h>
+#include <math.h>
 
 int Binary2Decimal(char * binary);
 
 int main() {
 
-	char * binaryStr[50];
+	char binaryStr[50];
 
-	printf("Enter binary string: ");
+	printf("Enter binary string: \n");
+	fflush(stdout);
    	scanf("%s", binaryStr);
 
-   	printf("The decimal value is: %i",Binary2Decimal(binaryStr));
+   	printf("The decimal value is: %i\n",Binary2Decimal(binaryStr));
 
 	return 0;
 }
 
 int Binary2Decimal(char * binary){
 
-	int numBits = 0;
+	int numBits = strlen(binary);
 	int decimal = 0;
 
-	while (binary[numBits] != "\0") numBits++;
+	//while (strcmp(binary+numBits,"\0")) numBits++;
 
 	for (int i=0; i<numBits;i++){
-		switch (binary[numBits-1-i]){
-			"1": decimal += 2^i;
-			"0": break;
-			default:
+
+		switch(binary[numBits-1-i]){
+			case '1' : decimal += pow(2,i);
+			/* no break */
+			case '0' : break;
+			default :
 				printf("Please only use 0 and 1 for inputs!\n");
 				return -1;
-		}
-	}
+		} // switch
+
+	} // for
 
 	return decimal;
 }
